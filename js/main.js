edda26d6ec9e2194bdc03de39ea9bbda199997d9
Ivàn Alfonso Villa Vàzquez <ivanalfonsovillavazquez@macbook-pro.gdl.mex.ibm.com>
2020-01-28 15:58:37 -0600

    version 1

--- js/main.js
+++ js/main.js
@@ -1,172 +1,129 @@
-jQuery(function($) {
+/*
+	Theme Custom Scripts
+*/
 
-	//Preloader
-	var preloader = $('.preloader');
-	$(window).load(function(){
-		preloader.remove();
-	});
+(function($){ "use strict";
 
-	//#main-slider
-	var slideHeight = $(window).height();
-	$('#home-slider .item').css('height',slideHeight);
-
-	$(window).resize(function(){'use strict',
-		$('#home-slider .item').css('height',slideHeight);
-	});
-	
-	//Scroll Menu
-	$(window).on('scroll', function(){
-		if( $(window).scrollTop()>slideHeight ){
-			$('.main-nav').addClass('navbar-fixed-top');
-		} else {
-			$('.main-nav').removeClass('navbar-fixed-top');
-		}
-	});
-	
-	// Navigation Scroll
-	$(window).scroll(function(event) {
-		Scroll();
-	});
-
-	$('.navbar-collapse ul li a').on('click', function() {  
-		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
-		return false;
-	});
-
-	// User define function
-	function Scroll() {
-		var contentTop      =   [];
-		var contentBottom   =   [];
-		var winTop      =   $(window).scrollTop();
-		var rangeTop    =   200;
-		var rangeBottom =   500;
-		$('.navbar-collapse').find('.scroll a').each(function(){
-			contentTop.push( $( $(this).attr('href') ).offset());
-			contentBottom.push( $( $(this).attr('href') ).offset() + $( $(this).attr('href') ).height() );
-		})
-		$.each( contentTop, function(i){
-			if ( winTop > contentTop[i] - rangeTop ){
-				$('.navbar-collapse li.scroll')
-				.removeClass('active')
-				.eq(i).addClass('active');			
-			}
-		})
-	};
-
-	$('#tohash').on('click', function(){
-		$('html, body').animate({scrollTop: $(this.hash).offset().top - 5}, 1000);
-		return false;
-	});
-	
-	//Initiat WOW JS
-	new WOW().init();
-	//smoothScroll
-	smoothScroll.init();
-	
-	// Progress Bar
-	$('#about-us').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-		if (visible) {
-			$.each($('div.progress-bar'),function(){
-				$(this).css('width', $(this).attr('aria-valuetransitiongoal')+'%');
-			});
-			$(this).unbind('inview');
-		}
-	});
-
-	//Countdown
-	$('#features').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-		if (visible) {
-			$(this).find('.timer').each(function () {
-				var $this = $(this);
-				$({ Counter: 0 }).animate({ Counter: $this.text() }, {
-					duration: 2000,
-					easing: 'swing',
-					step: function () {
-						$this.text(Math.ceil(this.Counter));
-					}
-				});
-			});
-			$(this).unbind('inview');
-		}
-	});
-
-	// Portfolio Single View
-	$('#portfolio').on('click','.folio-read-more',function(event){
-		event.preventDefault();
-		var link = $(this).data('single_url');
-		var full_url = '#portfolio-single-wrap',
-		parts = full_url.split("#"),
-		trgt = parts[1],
-		target_top = $("#"+trgt).offset().top;
-
-		$('html, body').animate({scrollTop:target_top}, 600);
-		$('#portfolio-single').slideUp(500, function(){
-			$(this).load(link,function(){
-				$(this).slideDown(500);
-			});
-		});
-	});
-
-	// Close Portfolio Single View
-	$('#portfolio-single-wrap').on('click', '.close-folio-item',function(event) {
-		event.preventDefault();
-		var full_url = '#portfolio',
-		parts = full_url.split("#"),
-		trgt = parts[1],
-		target_offset = $("#"+trgt).offset(),
-		target_top = target_offset.top;
-		$('html, body').animate({scrollTop:target_top}, 600);
-		$("#portfolio-single").slideUp(500);
-	});
-
-	// Contact form
-	var form = $('#main-contact-form');
-	form.submit(function(event){
-		event.preventDefault();
-		var form_status = $('<div class="form_status"></div>');
-		var values = {
-			"nombre": $("#nombre").val(),
-			"correo": $("#correo").val(),
-			"asunto": $("#asunto").val(),
-			"mensaje": $("#mensaje").val()
-		};
-		$.ajax({
-			type:"POST",
-			url: $(this).attr('action'),
-			data: values,
-			beforeSend: function(){
-				form.prepend( form_status.html('<p><i class="fa fa-spinner fa-spin"></i> Enviando Correo...</p>').fadeIn() );
-			}
-		}).done(function(response){
-			//$("#mails").html(response);
-			form_status.html('<p class="text-success">Gracias por contactarnos. En unos momentos nos comunicaremos contigo.</p>').delay(3000).fadeOut();
-		});
-	});
-
-	//Google Map
-	var latitude = $('#google-map').data('latitude')
-	var longitude = $('#google-map').data('longitude')
-	function initialize_map() {
-		var myLatlng = new google.maps.LatLng(latitude,longitude);
-		var mapOptions = {
-			zoom: 14,
-			scrollwheel: false,
-			center: myLatlng
-		};
-		var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
-		var contentString = '';
-		var infowindow = new google.maps.InfoWindow({
-			content: '<div class="map-content"><ul class="address">' + $('.address').html() + '</ul></div>'
-		});
-		var marker = new google.maps.Marker({
-			position: myLatlng,
-			map: map
-		});
-		google.maps.event.addListener(marker, 'click', function() {
-			infowindow.open(map,marker);
-		});
-	}
-	google.maps.event.addDomListener(window, 'load', initialize_map);
-	
+/*=========================================================================
+        Preloader
+=========================================================================*/
+$(window).on('load', function() {
+    $('body').addClass('loaded');
 });
 
+/*=========================================================================
+    Sticky Header
+=========================================================================*/ 
+    $(window).on('scroll', function() {
+
+        if ($(window).scrollTop() > 50 ) {
+            $('.header_area').addClass('scrollActive');
+        } else {
+            $('.header_area').removeClass('scrollActive');
+        }
+        
+    });
+             
+/*=========================================================================
+    Typed js Active
+=========================================================================*/
+    $(".typed").typed({
+        strings: ["Michel.", "Designer.", "Creative."],
+        loop: true,
+        typeSpeed: 150
+    });
+             
+/*=========================================================================
+    Initialize smoothscroll plugin
+=========================================================================*/
+    smoothScroll.init({
+		offset: 60
+	});
+             
+/*=========================================================================
+	Isotope Active
+=========================================================================*/
+	$('.portfolio_items').imagesLoaded( function() {
+
+		 // Add isotope click function
+		$('.portfolio_filter li').on( 'click', function(){
+	        $(".portfolio_filter li").removeClass("active");
+	        $(this).addClass("active");
+	 
+	        var selector = $(this).attr('data-filter');
+	        $(".portfolio_items").isotope({
+	            filter: selector,
+	            animationOptions: {
+	                duration: 750,
+	                easing: 'linear',
+	                queue: false,
+	            }
+	        });
+	        return false;
+	    });
+
+	    $(".portfolio_items").isotope({
+	        itemSelector: '.single_item',
+	        layoutMode: 'fitRows',
+	    });
+	});
+
+/*=========================================================================
+	Testimonial Carousel
+=========================================================================*/ 
+  var testiCarousel = $('#testi_carousel');
+  testiCarousel.owlCarousel({
+        loop: true,
+        autoplay: false,
+        smartSpeed: 800,
+        margin: 20,
+        responsive : {
+            // breakpoint from 0 up
+            0 : {
+                items: 1
+            },
+            // breakpoint from 480 up
+            520 : {
+                items: 1
+            },
+            // breakpoint from 768 up
+            768 : {
+                items: 3
+            }
+        }
+    });
+             
+/*=========================================================================
+    Active venobox
+=========================================================================*/
+	$('.img_popup').venobox();
+             
+/*=========================================================================
+    Skills Counter
+=========================================================================*/
+	$('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+
+/*=========================================================================
+	Scroll To Top
+=========================================================================*/ 
+    $(window).on( 'scroll', function () {
+        if ($(this).scrollTop() > 100) {
+            $('#scroll-to-top').fadeIn();
+        } else {
+            $('#scroll-to-top').fadeOut();
+        }
+    });	
+             
+/*=========================================================================
+    wow Settings
+=========================================================================*/
+    var wow = new WOW( {
+        mobile: false,
+        offset: 0
+    });
+    wow.init();
+
+})(jQuery);
